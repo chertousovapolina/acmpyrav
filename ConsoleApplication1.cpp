de16f94fe1d3bc2dba872a25c25ab2b838c6773d
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-20 16:40:34 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿//Чертоусова дз асмр уравнение
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	for (int x = (-100); x <= 100; x++)
+		if ((a * x * x * x + b * x * x + c * x + d) == 0) {
+			cout << x << " ";
+		}
+		
+	
+
+	
+
+}
+
+
